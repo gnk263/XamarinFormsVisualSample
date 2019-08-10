ac8541c6a06ba0cea2ec22daf18e98c4b85546b0
gnk263 <gnk263@users.noreply.github.com>
2019-08-10 17:04:39 +0900

    初期化コードを追加

--- XFVisualSample.iOS/AppDelegate.cs
+++ XFVisualSample.iOS/AppDelegate.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Xamarin.Forms;
 
 namespace XFVisualSample.iOS
 {
@@ -23,6 +24,7 @@ namespace XFVisualSample.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            FormsMaterial.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
