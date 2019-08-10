8d38a937c2a6f5fe1ad402b9efb277e549c9caa8
gnk263 <gnk263@users.noreply.github.com>
2019-08-10 18:38:50 +0900

    自作のRendererを追加

---
+++ XFVisualSample.Android/MyCustomButtonRenderer.cs
@@ -0,0 +1,23 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XFVisualSample;
+using XFVisualSample.Droid;
+using ButtonRenderer = Xamarin.Forms.Platform.Android.AppCompat.ButtonRenderer;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(MyCustomButtonRenderer), new[] { typeof(MyCustomVisual) })]
+namespace XFVisualSample.Droid
+{
+    public class MyCustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Control.SetBackgroundColor(Color.LightCoral.ToAndroid());
+            }
+        }
+    }
+}
---
+++ XFVisualSample.iOS/MyCustomButtonRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFVisualSample;
+using XFVisualSample.iOS;
+
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(MyCustomButtonRenderer), new[] { typeof(MyCustomVisual) })]
+namespace XFVisualSample.iOS
+{
+    public class MyCustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Control.BackgroundColor = Color.LightCoral.ToUIColor();
+            }
+        }
+    }
+}
